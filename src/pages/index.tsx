843126552b87fd0b9c9c6100e13d50596f17f0d2
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-05 13:14:06 +0700

    style: fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import { NextPageWithLayout } from './page'
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <main className='flex min-w-full justify-center '>
+      <section className='flex min-w-full justify-center '>
         <p className='text-2xl px-2'>next js starter by tri</p>
-      </main>
+      </section>
     </>
   )
 }
